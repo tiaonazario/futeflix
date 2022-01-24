3a986d9fbac1bba2da561807ae6574923278dc4a
tiaonazario <tiaogogo@gmail.com>
2022-01-24 00:53:36 -0300

    Seção time feita

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,13 @@ btnMenu.onclick = () => {
     btnMenu.classList.toggle('fa-times')
     menuPrincipal.classList.toggle('active')
 }
+
+const sectionTimes = document.querySelector('#times')
+const logoTime = document.querySelector('.logo-time')
+const nomeTime = document.querySelector('.nome-time')
+const descricaoTime = document.querySelector('.descricao-time')
+
+sectionTimes.style.backgroundImage = `linear-gradient(rgba(0,0,0,.9), rgba(0,0,0,.90)100%), url(${flamengo.fundo})`
+logoTime.src = flamengo.escudo
+nomeTime.textContent = flamengo.nome
+descricaoTime.textContent = flamengo.descricao
